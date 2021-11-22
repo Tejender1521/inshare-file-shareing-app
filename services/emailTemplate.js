e02341a17b8088c37db892eb6126fb6614680ea1
TEJENDER UPADHYAY <2020kucp1019@iiitkota.ac.in>
2021-11-22 20:46:56 +0530

    Name correction

--- services/emailTemplate.js
+++ services/emailTemplate.js
@@ -147,7 +147,7 @@ module.exports = ({emailFrom, downloadLink, size, expires}) => {
                         <table border="0" cellpadding="0" cellspacing="0" style="border-collapse: separate; mso-table-lspace: 0pt; mso-table-rspace: 0pt; width: 100%;">
                             <tr>
                             <td class="content-block" style="font-family: sans-serif; vertical-align: top; padding-bottom: 10px; padding-top: 10px; font-size: 12px; color: #999999; text-align: center;">
-                                <span class="apple-link" style="color: #999999; font-size: 12px; text-align: center;">Vidhusee Gourav Tejender</span>
+                                <span class="apple-link" style="color: #999999; font-size: 12px; text-align: center;">Vidushee Gourav Tejender</span>
                                 <br> Want to share a file? <a href="https://inshare-file-shareing-app.herokuapp.com/" style="text-decoration: underline; color: #999999; font-size: 12px; text-align: center;">inShare</a>.
                             </td>
                             </tr>
